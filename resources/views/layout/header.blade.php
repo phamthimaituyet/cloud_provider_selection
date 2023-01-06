c7ec554919974fc6daa617c5972b1213cd8892a9
phamthimaituyet <phamthimaituyet2000@gmail.com>
2023-01-06 20:00:25 +0700

    home main

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,17 +1,16 @@
-<div class="head">
-    <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top">
+        <div class="container">
             <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
                 <img src="assets/images/logo.png" alt="">
                 <span class="fs-4 text-white"><b>Trust Quality</b></span>
             </a>
-    
             <ul class="nav nav-pills">
                 <li class="nav-item"><a href="#" class="nav-link text-white" aria-current="page"><b>Home</b></a></li>
                 <li class="nav-item"><a href="#" class="nav-link text-white"><b>Cloud services</b></a></li>
                 <li class="nav-item"><a href="#" class="nav-link text-white"><b>Sign up</b></a></li>
                 <li class="nav-item"><a href="#" class="nav-link text-white"><b>Log in</b></a></li>
             </ul>
-        </header>
-    </div>
-</div>
+        </div>
+    </nav>
+</header>
